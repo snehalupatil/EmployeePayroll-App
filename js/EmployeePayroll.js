fd45fe7efeb23f05ef2870744c6d466db2675f8c
snehalupatil <48207533+snehalupatil@users.noreply.github.com>
2021-06-13 18:22:23 +0530

    Update EmployeePayroll.js

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,11 +1,5 @@
 class EmployeePayroll{
 
-    //getter and Setter for id
-    get id() {return this.id;}
-    set id(id){
-        this._id = id;
-    }
-
     //getter and Setter for name
     get name() {return this._name;}
     set name(name){
@@ -16,35 +10,7 @@ class EmployeePayroll{
             throw "Name is incorrect";
     }
 
-    //getter and Setter for profilePic
-    get profilePic() {return this._profilePic};
-    set profilrPic(profilePic){
-        this._profilePic = profilePic;
-    }
-
-    //getter and Setter for gender
-    get gender() {return this._gender;}
-    set gender(gender){
-        this._gender = gender;
-    }
-
-    //getter and Setter for department
-    get department() {return this._departmetnt;}
-    set department() {
-        this._departmetnt = this.department;
-    }
-
-    //getter and Setter for note
-    get note() {return this._note;}
-    set note(note){
-        this._note = note;
-    }
-
-    //getter and Setter for salary
-    get salary() { return this._salary;}
-    set salary(salary){
-        this._salary = salary;
-    }
+    
 
     //getter and Setter for startDate
     get startDate() {return this.startDate;}
