53b7cebbc57a91b57fe7f4c85452ba0ff8b4dd2d
snehalupatil <patilsnehal4575@gmail.com>
2021-06-17 13:56:24 +0530

    Added: Display Employee Details from Local Storage

---
+++ js/payroll_form.js
@@ -0,0 +1,206 @@
+let isUpdate = false;
+let employeePayrollObj = {};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name=document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            textError.textContent="";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent="";
+        }catch (e) {
+            textError.textContent=e;
+        } 
+    });   
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+    output.textContent = salary.value; 
+    }); 
+
+    const date = document.querySelector('#date');
+    const dateError = document.querySelector('.date-error');
+    date.addEventListener('input', function () {
+        const startDate = getInputValueById('#day')+" "+
+                          getInputValueById('#month')+" "+
+                          getInputValueById('#year');
+        try {
+            checkStartDate(new Date(Date.parse(startDate)));
+            dateError.textContent="";
+        }catch (e) {
+            dateError.textContent=e;
+        }
+    });
+
+    checkForUpdate();
+});
+
+const save = (event) => {
+    event.preventDefault();
+    event.stopPropogation();
+    try {
+        setEmployeePayrollObject();
+        createAndUpdateStorage();
+        resetForm();
+        window.location.replace(site_properties.home_page);
+    } catch (e) {
+        return;
+    }
+    // try{
+    //     let employeePayrollData = createEmployeePayroll();
+    //     createAndUpdateStorage(employeePayrollData);
+    // }catch(e){
+    //     return;
+    // }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name=getInputValueById('#name');
+    } catch(e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+//     employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+//     employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+//     employeePayrollData.department = getSelectedValues('[name=department]');
+//     employeePayrollData.salary = getInputValueById('#salary');
+//     employeePayrollData.note = getInputValueById('#notes');
+//     let date = getInputValueById('#day') +" "+getInputValueById('#month')+" "+getInputValueById('#year');
+//     employeePayrollData.date = Date.parse(date);
+//     alert(employeePayrollData.toString());
+//     return employeePayrollData;
+
+}
+
+const setEmployeePayrollObject = () => {
+    employeePayrollObj.name = getInputValueById('#name');
+    employeePayrollObj._profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollObj._gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollObj._department = getSelectedValues('[name=department]');
+    employeePayrollObj._salary = getInputValueById('#salary');
+    employeePayrollObj._note = getInputValueById('#notes');
+    let date = getInputValueById('#day') +" "+getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollObj._date = date;
+}
+
+const  getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems= [];
+    allItems.forEach(item => {
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+/**
+ * 1: querrySelector is the newer feature.
+ * 2: The querySelector method can be used when selecting by element name,
+ *    nesting, or class name.
+ * 3: quereySelector lets you find elements with rules that cant be
+ *    expressed with getElementByID.
+ */
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+/**
+ * 1: getElemtByID is better supported than quereySelector in older versions
+ *    of the Browser
+ * 2: The thing with getElementByID is that, it only allows to select an
+ *    element by its ID
+ */
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+const checkStartDate = (startDate) => {
+    let now = new Date();
+    if (startDate > now) throw 'Start Date is Future Date!';
+}
+
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList=[employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
+}
+
+const resetForm = () => {
+    setValue("#name", '');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setSelectedIndex('#day',0);
+    setSelectedIndex('#month', 0);
+    setSelectedIndex('#year',0);
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    })
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value ) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
+
+const checkForUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson ? true : false;
+    if(!isUpdate) return;
+    employeePayrollObj = JSON.parsel(employeePayrollJson);
+    setForm();
+}
+
+const setform = () => {
+    setValue('#name', employeePayrollObj._name);
+    setSelectedValues('[name=profile]',employeePayrollObj._profilePic);
+    setSelectedValues('[name=gender]',employeePayrollObj._gender);
+    setSelectedValues('[name=department]',employeePayrollObj._department);
+    setValues('#salary',employeePayrollObj._salary);
+    setTextValue('.salary-output', employeePayrollObj._salary);
+    setValue('#notes', employeePayrollObj._note);
+    let date = stringifyDate(employeePayrollObj._startDate).split(" ");
+    setValue('#day', date[0]);
+    setValue('#month', date[1]);
+    setValue('#year', date[2]);
+}
+
+const setSelectedValues = (propertyValue, value) => {
+    let allItems = document.quereySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        if(Array.isArray(value)) {
+            if (value.includes(item.value)) {
+                item.checked = true;
+            }
+        }
+        else if (item.value == value)
+            item.checked = true;
+    });
+}
---
+++ js/site_properties.js
@@ -0,0 +1,4 @@
+var site_properties = {
+    home_page: "../pages/home.html",
+    add_emp_payroll_page: "../pages/payroll_form.html",
+};
